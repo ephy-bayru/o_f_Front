ac73762f1af764043e67152e7e234de376504dff
Ephrem <ephybayru@gmail.com>
2023-05-14 13:07:27 +0300

    feat: Set up routing in AuthModule. \n\n1. Created LoginComponent, RegisterComponent, ChangePasswordComponent, and ResetPasswordComponent. \n2. Set up routing for each of these components in AuthRoutingModule.

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
