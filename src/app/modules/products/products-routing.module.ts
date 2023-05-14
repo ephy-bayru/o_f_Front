e181d5690ccc636ea5bfb513110d32b2e0f437fe
Ephrem <ephybayru@gmail.com>
2023-05-14 13:09:26 +0300

    feat: Set up routing in ProductsModule. \n\n1. Created ProductListComponent and ProductDetailComponent. \n2. Set up routing for each of these components in ProductsRoutingModule.

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ProductListComponent },
+  { path: 'detail/:id', component: ProductDetailComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
