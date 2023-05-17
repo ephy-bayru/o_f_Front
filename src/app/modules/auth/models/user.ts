23dd0343ac47b91a768a01140bc03e177877fcc7
Ephrem <ephybayru@gmail.com>
2023-05-17 20:20:51 +0300

    Feat: User interface

--- src/app/modules/auth/models/user.ts
+++ src/app/modules/auth/models/user.ts
@@ -1,2 +1,37 @@
-export interface User {
-}
+export interface IUser {
+    _id: string;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+    role: IUserRole;
+    permissions: string[];
+    suspended: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
+  
+  export interface IUserPayload extends Partial<IUser> {}
+  
+  export enum IUserRole {
+    User = 'user',
+    Admin = 'admin'
+  }
+  
+  export interface IAuth {
+    email: string;
+    password: string;
+  }
+  
+  export interface IAuthResponse {
+    status: string;
+    message: string;
+    data: {
+      user: IUser;
+      accessToken: string;
+      refreshToken: string;
+    };
+  }
+  
