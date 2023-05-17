914f36ded894d09f33d89c5ea422ec80aa56e30d
Ephrem <ephybayru@gmail.com>
2023-05-17 20:18:51 +0300

    Feat: notification service

--- src/app/core/services/notification.service.ts
+++ src/app/core/services/notification.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
+
+  showSuccess(message: string, duration: number = 5000): void {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      panelClass: ['success-snackbar']
+    });
+  }
+
+  showError(message: string, duration: number = 5000): void {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      panelClass: ['error-snackbar']
+    });
+  }
+
+  showInfo(message: string, duration: number = 5000): void {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      panelClass: ['info-snackbar']
+    });
+  }
+
+  showWarning(message: string, duration: number = 5000): void {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      panelClass: ['warning-snackbar']
+    });
+  }
+
 }
