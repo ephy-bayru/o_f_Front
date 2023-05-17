54e964f8f5ffac7b0aae74476800382e201575db
Ephrem <ephybayru@gmail.com>
2023-05-17 20:18:30 +0300

    Feat: product details

--- src/app/modules/products/components/product-detail/product-detail.component.ts
+++ src/app/modules/products/components/product-detail/product-detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from '../../models/product';
+import { ProductService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product!: IProduct;
 
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const productId = params['id'];
+  
+      this.productService.getProduct(productId).subscribe(product => {
+        this.product = product;
+      });
+    });
+  } 
 }
