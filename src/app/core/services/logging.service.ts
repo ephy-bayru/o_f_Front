b598985f024d004d58d319ca6ffd3813eabf86c5
Ephrem <ephybayru@gmail.com>
2023-05-17 20:16:42 +0300

    Feat: logging.

--- src/app/core/services/logging.service.ts
+++ src/app/core/services/logging.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
 
+export enum LogLevel {
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoggingService {
+  logLevel = LogLevel.DEBUG;
 
-  constructor() { }
+  constructor() {}
+
+  debug(message: string, ...optionalParams: any[]) {
+    if (this.shouldLog(LogLevel.DEBUG)) {
+      console.debug('DEBUG: ' + message, optionalParams);
+    }
+  }
+
+  info(message: string, ...optionalParams: any[]) {
+    if (this.shouldLog(LogLevel.INFO)) {
+      console.info('INFO: ' + message, optionalParams);
+    }
+  }
+
+  warn(message: string, ...optionalParams: any[]) {
+    if (this.shouldLog(LogLevel.WARN)) {
+      console.warn('WARN: ' + message, optionalParams);
+    }
+  }
+
+  error(message: string, ...optionalParams: any[]) {
+    if (this.shouldLog(LogLevel.ERROR)) {
+      console.error('ERROR: ' + message, optionalParams);
+    }
+  }
+
+  private shouldLog(level: LogLevel): boolean {
+    return level >= this.logLevel;
+  }
 }
