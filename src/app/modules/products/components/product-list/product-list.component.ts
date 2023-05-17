666b25d3048adc864258399d7793105355340a1e
Ephrem <ephybayru@gmail.com>
2023-05-17 20:18:08 +0300

    Feat: product list

--- src/app/modules/products/components/product-list/product-list.component.ts
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { IProduct, IProductWithPosition } from '../../models/product';
+import { ProductService } from '../../services/products.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = [
+    'select',
+    'position',
+    'name',
+    'description',
+    'price',
+    'stock',
+    'rating',
+    'details',
+  ];
+  selection = new SelectionModel<IProduct>(true, []);
+  products: MatTableDataSource<IProductWithPosition> =
+    new MatTableDataSource<IProductWithPosition>([]);
+  totalCount: number = 0;
+  pageSize: number = 10;
+  currentPage: number = 1;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private productService: ProductService,
+    private notificationService: NotificationService, private changeDetectorRefs: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+   // this.loadProducts();
+  }
+
+  ngAfterViewInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getAllProducts(this.pageSize, this.currentPage).subscribe({
+      next: (res) => {
+        this.totalCount = res.data.totalCount;
+        this.products = new MatTableDataSource<IProductWithPosition>(res.data.products.map((product, index) => {
+          return {...product, position: ((this.currentPage - 1) * this.pageSize) + index + 1};
+        }));
+  
+        this.products.sort = this.sort;
+  
+        this.changeDetectorRefs.detectChanges();
+      },
+      error: (error) => {
+        console.error(error);
+        this.notificationService.showError('An error occurred while loading the products.');
+      }
+    });
+  }
+  
+
+  pageEvent(event: any): void {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex + 1; // pageIndex is 0-based
+    this.loadProducts();
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.products.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.products.data.forEach((row) => this.selection.select(row));
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.products.filter = filterValue.trim().toLowerCase();
+
+    if (this.products.paginator) {
+      this.products.paginator.firstPage();
+    }
+  }
 }
--- src/app/modules/products/models/product.ts
+++ src/app/modules/products/models/product.ts
@@ -1,2 +1,39 @@
-export interface Product {
-}
+export interface IProduct {
+    _id: string;
+    name: string;
+    description: string;
+    category: string;
+    subcategory?: string;
+    price: number;
+    salePrice?: number;
+    stock: number;
+    sku: string;
+    images?: string[];
+    isFeatured: boolean;
+    isPublished: boolean;
+    rating: number;
+    totalReviews: number;
+    tags?: string[];
+    brand: string;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+  export interface IProductResponse {
+    status: string;
+    message: string;
+    data: {
+      totalCount: number;
+      products: IProduct[];
+      pagination: {
+        totalPages: number;
+        currentPage: number;
+        nextPage: boolean;
+        previousPage: boolean;
+        pageSize: number;
+      };
+    };
+  }
+  
+  export interface IProductWithPosition extends IProduct {
+    position: number;
+  }
