507ddc03bae1f79dcd15b7c6c70d44cc38b0f070
Ephrem <ephybayru@gmail.com>
2023-05-17 20:08:29 +0300

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,57 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './layouts/login/login.component';
+import { MainComponent } from './layouts/main/main.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { RoleGuard } from './core/guards/role.guard';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
 
 const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'products',
-    loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule)
+    path: '',
+    component: LoginComponent,
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () =>
+          import('./modules/auth/auth.module').then((m) => m.AuthModule),
+      },
+      { path: '', redirectTo: 'auth', pathMatch: 'full' },
+    ],
   },
   {
     path: '',
-    redirectTo: '', 
-    pathMatch: 'full'
+    component: MainComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('./modules/products/products.module').then(
+            (m) => m.ProductsModule
+          ),
+        canActivate: [RoleGuard],
+        data: { role: 'admin' },
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('./modules/profile/profile.module').then(
+            (m) => m.ProfileModule
+          ),
+        canActivate: [RoleGuard],
+        data: { role: 'user' },
+      },
+      { path: '', redirectTo: 'products', pathMatch: 'full' },
+    ],
   },
-  { path: '**', component: PageNotFoundComponent }
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
