d0811931cf88c48aede2128b21bb458249e7f9b8
Ephrem <ephybayru@gmail.com>
2023-05-17 20:22:05 +0300

    Feat: products module

--- src/app/modules/products/products.module.ts
+++ src/app/modules/products/products.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductCardComponent
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    SharedModule
   ]
 })
 export class ProductsModule { }
